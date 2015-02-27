cee2b8a3b828a7b19824f8af78ac624377ee77a3
Niket Shah <masterniket@gmail.com>
2015-02-27 11:57:45 +0000

    test 9

--- spec/simple-request-spec.js
+++ spec/simple-request-spec.js
@@ -4,7 +4,7 @@ var ip = require('ip');
 describe("a simple test suite", function() {
 	it("should repond with 'Hello Niket!'", function(done) {
 		console.log(ip.address());
-		request(ip.address() + ":3000/name/Niket", function(error, response, body) {
+		request("http://localhost" + ":3000/name/Niket", function(error, response, body) {
 			console.log(error);
 			console.log("request: " + JSON.stringify(response));
 			expect(body).toEqual("Hello Niket!\n\n");
